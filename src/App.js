d4ad60e1f55bf16c33b15252027263a9eb6b97fb
Luyao Wang <luwa@microsoft.com>
2019-10-02 17:46:02 -0700

    added msal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import MSAL from 'msal';
+import React from 'react';
+
+import logo from './logo.svg';
+
+onSignInClick = () =>
+{
+  alert('hey');
+  const config =
+  {
+    auth:
+    {
+      clientId: 'bf04bdab-e06f-44f3-9821-d3af64fc93a9',
+      authority: `https://login.microsoftonline.com/common`,
+      redirectUri: 'https:raas.local',
+      postLogoutRedirectUri: window.location.origin,
+    },
+    cache:
+    {
+      cacheLocation: 'localStorage'
+    },
+    system:
+    {
+      loadFrameTimeout: 30000
+    }
+  };
+  const msal = new MSAL.UserAgentApplication(config);
+  msal.loginRedirect()
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>luwa</p>
+        <button onClick={ onSignInClick }>Sign in</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +48,7 @@ function App() {
       </header>
     </div>
   );
+
 }
 
 export default App;
